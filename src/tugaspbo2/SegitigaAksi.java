68b13f26147ed1bd313a3dbc183bb1aeb05998ce
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-04-01 22:54:52 +0700

    Mengisi Nilai atribute

--- src/tugaspbo2/SegitigaAksi.java
+++ src/tugaspbo2/SegitigaAksi.java
@@ -3,6 +3,8 @@ package tugaspbo2;
 public class SegitigaAksi {
     public static void main(String[] args) {
         Segitiga s1 = new Segitiga();
+        s1.alas = 17;
+        s1.tinggi = 22;
     }
     
 }
