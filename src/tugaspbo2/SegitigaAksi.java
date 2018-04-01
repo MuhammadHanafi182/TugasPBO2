ac068d30730421e7a7464302fd382b9da4933605
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-04-01 23:23:20 +0700

    Constructor dengan Parameter

--- src/tugaspbo2/SegitigaAksi.java
+++ src/tugaspbo2/SegitigaAksi.java
@@ -9,6 +9,12 @@ public class SegitigaAksi {
         s1.cetakinfo();
         System.out.println("Luas Segitiga ="+s1.hitungluas());
         s1.cetakluas();
+        
+        Segitiga s2 = new Segitiga();
+        s2.cetakinfo();
+        
+        Segitiga s3 = new Segitiga(25,40);
+        s3.cetakinfo();
     }
     
 }
