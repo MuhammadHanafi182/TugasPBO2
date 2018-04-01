db5f8344d11891564702f05f98cee9b495555117
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-04-01 22:49:41 +0700

    Membuat atribut dan Behavior

--- src/tugaspbo2/Segitiga.java
+++ src/tugaspbo2/Segitiga.java
@@ -1,4 +1,13 @@
 package tugaspbo2;
 public class Segitiga {
+    double alas;
+    double tinggi;
+    
+    void cetakinfo(){
+        System.out.println("=========================");
+        System.out.println("Alas   :"+alas);
+        System.out.println("Tinggi :"+tinggi);
+        System.out.println("=========================");
+    }
     
 }
